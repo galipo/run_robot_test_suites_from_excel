220ab1a58a468ea7a530af33e3fcee9a91e09e54
Hasan Fatih Topal <139459390+fatih-tpl@users.noreply.github.com>
2024-08-22 13:48:23 +0300

    Delete Ulak_Tasks_1 directory

--- Ulak_Tasks_1/task_write_text.py
+++
@@ -1,7 +0,0 @@
-f = open("ArgumentFile.txt","w")     #opening for writing the file
-f.write("deneme 1")                  #writing into the file
-f.close()                            #closing to the file
-
-f = open("ArgumentFile.txt", "r")    #After opening for reading
-print(f.read())                      #Reading to the file
-
